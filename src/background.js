11da5407e11fdc3556eed6e2cae6809bbdd7f176
Andrei <andrei@laye.red>
2024-10-06 13:01:20 +0200

    added: context menu to save in app

--- src/background.js
+++ src/background.js
@@ -1,18 +1,26 @@
 let pocketId = null
 
-chrome.storage.sync.get('distinct_id').then((kv) => {
-	if (kv.distinct_id) {
-		pocketId = kv.distinct_id
-	} else {
-		pocketId = crypto.randomUUID()
-		chrome.storage.sync.set({ distinct_id: pocketId })
-	}
-})
+async function getPocketId() {
+	if (!pocketId) {
+		const kv = await chrome.storage.sync.get('distinct_id')
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-	if (typeof message === 'string' && message === 'get-pocket-id') {
-		sendResponse(pocketId)
+		if (kv.distinct_id) {
+			pocketId = kv.distinct_id
+		} else {
+			pocketId = crypto.randomUUID()
+			chrome.storage.sync.set({ distinct_id: pocketId })
+		}
 	}
+
+	return pocketId
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	if (typeof message === 'string' && message === 'get-pocket-id') {
+		getPocketId().then(sendResponse)
+	}
+
+	return true
 });
 
 chrome.runtime.onInstalled.addListener((details) => {
@@ -39,24 +47,26 @@ chrome.runtime.onInstalled.addListener((details) => {
 
 	chrome.contextMenus.create({
 		id: 'snapshot-link',
-		title: 'Save in Snapshot',
+		title: 'Save in Hono',
 		contexts: ['link', 'page']
 	});
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
 	if (info.menuItemId === 'snapshot-link') {
-		fetch(`https://pocket-api.unallow.com/pockets/${pocketId}/save-url`, {
-			method: 'POST',
-			body: JSON.stringify({ url: info.linkUrl }),
-			headers: {
-				'authorization': `Bearer ${import.meta.env.VITE_API_TOKEN}`,
-				'content-type': 'application/json',
-				'x-platform': 'chrome_extension',
-				'x-version': '1.10.0',
-			},
+		getPocketId().then((pocketId) => {
+			fetch(`https://pocket-api.unallow.com/pockets/${pocketId}/save-url`, {
+				method: 'POST',
+				body: JSON.stringify({ url: info.linkUrl }),
+				headers: {
+					'authorization': `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+					'content-type': 'application/json',
+					'x-platform': 'chrome_extension',
+					'x-version': '1.10.0',
+				},
+			})
+	
+			chrome.tabs.create({ url: `https://myhono.com/set-pocket/${pocketId}` });
 		})
-
-		chrome.tabs.create({ url: `https://pocket-web.unallow.com/set-pocket/${pocketId}` });
 	}
 });
